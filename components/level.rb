e27d34b171c7a341a62c2f0adf347705659a3afb
Chadowo <83732118+Chadowo@users.noreply.github.com>
2023-08-15 17:11:05 -0300

    New level component

---
+++ components/level.rb
@@ -0,0 +1,46 @@
+
+class Level
+  attr_reader :map, :player
+
+  def initialize(map)
+    @map = map
+
+    @player = nil
+    @boxes = []
+  end
+
+  # Returns a new game level with game objects
+  def populate
+    @map.map do |row|
+      row.map do |cell|
+        case cell
+        when 0
+          Empty.new
+        when 1
+          Wall.new
+        when 2
+          @boxes << Box.new
+
+          # Grab the box we pushed
+          @boxes.last
+        when 3
+          Storage.new
+        when 4
+          @player = Pusher.new
+        else
+          cell
+        end
+      end
+    end
+  end
+
+  # Set the level to its initial condition
+  def reset
+    @boxes = []
+  end
+
+  # Are all the boxes of the level stashed?
+  def finished?
+    true if @boxes.all?(&:is_on_storage)
+  end
+end
